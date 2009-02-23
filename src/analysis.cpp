466b9122d8ea03d7d4945b6c7c92b38a738775c8
perarnau <perarnau@0533444f-c493-4ece-a72f-de083fb4a2d6>
2009-02-23 20:33:44 +0000

    Real implementation of the graph property analysis program
    This patch starts a simple implementation allowing
    the computation of the mean and standard deviation
    of one property
    
    git-svn-id: svn+ssh://svn.ligforge.imag.fr/svnroot/ggen/trunk@34 0533444f-c493-4ece-a72f-de083fb4a2d6

--- src/analysis.cpp
+++ src/analysis.cpp
@@ -18,49 +18,134 @@
 #include <boost/graph/graphviz.hpp>
 #include <boost/graph/properties.hpp>
 
+#include <boost/program_options.hpp>
 
+#include <iostream>
+
+
+/* GSL statistic functions
+ * GSL is prefered over boost accumulators as boost seems to
+ * compute statistics by approximation using a cache of values
+ */
+#include "gsl/gsl_statistics.h"
+
+
+#include "ggen.hpp"
 #include "dynamic_properties.hpp"
 
 using namespace boost;
-
-
-/* This is the definition of the graph struture
- * According to boost that means :
- *	* The graph is an adjacency list
- *	* The vertices are managed as a std::Vector
- *	* The edges are managed as std::set to enforce no parallel edges
- *	* The graph is bidirectional
- *	* We don't have any additional properties on vertices or edges
- */
-typedef adjacency_list<setS, vecS, bidirectionalS,
-	dynamic_properties, dynamic_properties> Graph;
+using namespace std;
 
 ////////////////////////////////////////////////////////////////////////////////
 // MAIN
 ////////////////////////////////////////////////////////////////////////////////
 
-dynamic_properties properties;
-Graph *g;
+namespace po = boost::program_options;
 
+dynamic_properties properties(&create_property_map);
+Graph *g;
 
 /* Main program
  */
 int main(int argc, char** argv)
 {
-	int nb_vertices,nb_edges;
+	istream *infile = NULL;
+	bool edge_property;
+	string name;
 
+	// Handle command line arguments
+	////////////////////////////////////
+	po::options_description desc("Allowed options");
+	desc.add_options()
+		("help", "produce this help message")
+		
+		/* I/O options */
+		("input,i", po::value<string>(), "Set the input file")
+
+		/* Property options */
+		("name,n",po::value<string>(),"Set the property name to analyse")
+		("edge,e",po::value<bool>()->zero_tokens(),"Analyse an edge property instead of a vertex one")
+	;
+
+	po::options_description all;
+	all.add(desc);
+	
+	
+	// Parse command line options
+	////////////////////////////////
+	po::variables_map vm;
+	po::store(po::parse_command_line(argc,argv,all),vm);
+	po::notify(vm);
+	
+	if (vm.count("help"))
+	{
+		std::cout << all << "\n";
+		        return 1;
+	}
+
+	if (vm.count("input")) 
+	{
+		filebuf *fb = new filebuf();
+		fb->open(vm["input"].as<std::string>().c_str(),ios::in);
+		infile = new istream(fb);
+	}
+	else
+		infile = &cin;
+
+	if(vm.count("name"))
+	{
+		name = vm["name"].as<string>();
+	}
+	else
+		name = "node_id";
+
+	if(vm.count("edge"))
+	{
+		edge_property = true;
+	}
+	else
+		edge_property = false;
 
 	// Graph generation
 	////////////////////////////////
 	
 	g = new Graph();
-	create_default_vertex_property(properties,*g);
 
-	// Write graph
+	// Read graph
 	////////////////////////////////////	
-	read_graphviz(std::cin, *g,properties);
+	read_graphviz(*infile, *g,properties);
+	
+	// Statistics
+	// What todo : 	push_back values needed
+	// 		convert the vector to call gsl
+	// 		print the values
+	double mean;
+	std::vector<double> values;
+	if(edge_property)
+	{
+		// iterate over edges and push_back values
+		std::pair<Edge_iter, Edge_iter> ep;
+		for (ep = boost::edges(*g); ep.first != ep.second; ++ep.first)
+			values.push_back(boost::lexical_cast<double>(get(name,properties,*ep.first)));
+	}
+	else
+	{
+		// iterate over vertices and push_back values
+		std::pair<Vertex_iter, Vertex_iter> vp;
+		for (vp = boost::vertices(*g); vp.first != vp.second; ++vp.first)
+			values.push_back(boost::lexical_cast<double>(get(name,properties,*vp.first)));
+	}
 
-	write_graphviz(std::cout, *g,properties);
+	double *data = new double [values.size()];
+	copy( values.begin(), values.end(), data);
+	
+       	mean = gsl_stats_mean(data, 1, values.size());
+	
+	std::cout << "Statistics for property " << name << " \n";
+	std::cout << "Mean : " << mean << " \n";
+	std::cout << "Standard Deviation : " << gsl_stats_sd_m(data,1,values.size(),mean) << " \n";	
+
+	delete [] data;
 	delete g;
 	return 0;
 }
