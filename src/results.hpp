a927c7b1b6f7bf2d5a5c5e46c6515c08c437088c
perarnau <perarnau@0533444f-c493-4ece-a72f-de083fb4a2d6>
2009-11-19 10:08:11 +0000

    more code and doc cleanups
    git-svn-id: svn+ssh://svn.ligforge.imag.fr/svnroot/ggen/branches/unstable@180 0533444f-c493-4ece-a72f-de083fb4a2d6

--- src/results.hpp
+++ src/results.hpp
@@ -1,25 +1,25 @@
 /* Copyright Swann Perarnau 2009
  *
- *   contributor(s) :  
+ *   contributor(s) :
  *
- *   contact : firstname.lastname@imag.fr	
+ *   contact : firstname.lastname@imag.fr
  *
  * This software is a computer program whose purpose is to help the
  * random generation of graph structures and adding various properties
  * on those structures.
  *
  * This software is governed by the CeCILL  license under French law and
- * abiding by the rules of distribution of free software.  You can  use, 
+ * abiding by the rules of distribution of free software.  You can  use,
  * modify and/ or redistribute the software under the terms of the CeCILL
  * license as circulated by CEA, CNRS and INRIA at the following URL
- * "http://www.cecill.info". 
- * 
+ * "http://www.cecill.info".
+ *
  * As a counterpart to the access to the source code and  rights to copy,
  * modify and redistribute granted by the license, users are provided only
  * with a limited warranty  and the software's author,  the holder of the
  * economic rights,  and the successive licensors  have only  limited
- * liability. 
- * 
+ * liability.
+ *
  * In this respect, the user's attention is drawn to the risks associated
  * with loading,  using,  modifying and/or developing or reproducing the
  * software by the user in light of its specific status of free software,
@@ -27,10 +27,10 @@
  * therefore means  that it is reserved for developers  and  experienced
  * professionals having in-depth computer knowledge. Users are therefore
  * encouraged to load and test the software's suitability as regards their
- * requirements in conditions enabling the security of their systems and/or 
- * data to be ensured and,  more generally, to use and operate it in the 
- * same conditions as regards security. 
- * 
+ * requirements in conditions enabling the security of their systems and/or
+ * data to be ensured and,  more generally, to use and operate it in the
+ * same conditions as regards security.
+ *
  * The fact that you are presently reading this means that you have had
  * knowledge of the CeCILL license and that you accept its terms.
  */
@@ -59,7 +59,8 @@ using namespace std;
 namespace ggen {
 
 /**
- * @file This file defines helper classes to compute statistics on the fly when analysing a graph.
+ * @file results.hpp
+ * This file defines helper classes to compute statistics on the fly when analysing a graph.
  */
 
 /** A result dumper
@@ -67,11 +68,11 @@ namespace ggen {
  */
 class ggen_result {
 	public:
-		/** Set the ostream to use for dump 
+		/** Set the ostream to use for dump
 		  * @param out the ostream to use for dumping result */
 		void set_stream(ostream *out);
 
-		/** dumps the result 
+		/** dumps the result
 		 * @param properties this is needed to acces names of vertices in most results*/
 		virtual void dump(dynamic_properties *properties) = 0;
 	protected:
@@ -85,9 +86,8 @@ class ggen_result {
 /** Specialized result, used to save a graph */
 class ggen_result_graph : public ggen_result {
 	public:
-		/** save a graph 
+		/** save a graph
 		 * @param graph the graph to save
-		 * @param properties the graph properties associated
 		 */
 		virtual void save(Graph *graph) = 0;
 };
@@ -120,7 +120,7 @@ class ggen_rg_stupid : public ggen_result_graph {
 class ggen_result_paths : public ggen_result {
 	public:
 		/** saves a path
-		 * @param path the path to save 
+		 * @param path the path to save
 		 */
 		virtual void save(std::list<Vertex> path) = 0;
 };
@@ -138,7 +138,7 @@ class ggen_rp_stupid : public ggen_result_paths {
 		~ggen_rp_stupid();
 
 		/** saves a path
-		 * @param path the path to save 
+		 * @param path the path to save
 		 */
 		void save(std::list<Vertex> path);
 
@@ -164,7 +164,7 @@ class ggen_result_vmap : public ggen_result {
 		virtual void save(Vertex key, string value) = 0;
 };
 
-/** A stupid implementation of ggen_result_vmap 
+/** A stupid implementation of ggen_result_vmap
  * Only saves all key/values pairs and dumps them as following:
  * key : value
  * */
