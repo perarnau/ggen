c6c64aab88447d7a5b907620910f8f3f848d6250
Swann Perarnau <swann.perarnau@imag.fr>
2009-11-30 15:20:22 +0100

    error handling for autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-
 set -x
-aclocal -I m4
-autoheader
-libtoolize
-automake --add-missing --copy
-autoconf
+aclocal -I m4 || exit 1
+autoheader || exit 1
+libtoolize || exit 1
+automake --add-missing --copy || exit 1
+autoconf || exit 1
