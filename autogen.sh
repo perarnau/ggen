c275ae8be1ef2f11b4f49c357a972d21781059ea
Swann Perarnau <swann.perarnau@imag.fr>
2009-12-20 17:15:45 +0100

    Add missing directory creation

--- autogen.sh
+++ autogen.sh
@@ -5,4 +5,6 @@ aclocal -I m4 || exit 1
 autoheader || exit 1
 libtoolize || exit 1
 automake --add-missing --copy || exit 1
+# autoconf needs build-aux directory
+mkdir -p build-aux || exit 1
 autoconf || exit 1
