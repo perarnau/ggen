fcbf5305bd13512cb2ef5fc22dbcf2a26bb82eea
Swann Perarnau <swann.perarnau@imag.fr>
2011-01-19 10:56:04 +0100

    [fix] create build-aux sooner

--- autogen.sh
+++ autogen.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -x
+# auto* need build-aux directory
+mkdir -p build-aux || exit 1
 sh version.sh
 aclocal -I m4 || exit 1
 autoheader || exit 1
 libtoolize || exit 1
 automake --add-missing --copy || exit 1
-# autoconf needs build-aux directory
-mkdir -p build-aux || exit 1
 autoconf || exit 1
