0bce372352ff4fe837c6901475291f7689e7e0f5
Swann Perarnau <perarnau@riken.jp>
2014-01-14 10:21:29 +0900

    bugfix: version.sh should always work
    the --always flag ensure that any git repo has a version,
    even one where no tags were imported (current github does not contain tags).

--- version.sh
+++ version.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if git describe --tags > version.tmp
+if git describe --tags --always > version.tmp
 then
     echo "m4_define([VERSION_NUMBER], [`tr -d '\n' < version.tmp`])" > version.m4
 fi
