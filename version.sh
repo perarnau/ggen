8edb9d484bfbea72bb4f0facbf3a5885b5d78635
Swann Perarnau <swann.perarnau@gmail.com>
2016-02-17 15:55:19 -0600

    [fix] version.sh should work on non-git directories
    If someone downloads a tarball of the git repo, without the git metadata (e.g.
    github tarballs), git describe will fail. Provide a 'local' version in such
    case.

--- version.sh
+++ version.sh
@@ -3,5 +3,7 @@
 if git describe --tags --always > version.tmp
 then
     echo "m4_define([VERSION_NUMBER], [`tr -d '\n' < version.tmp`])" > version.m4
+else
+    echo "m4_define([VERSION_NUMBER], [local])" > version.m4
 fi
 rm version.tmp 
